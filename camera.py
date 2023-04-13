e56cb238b3204dd3d138b3bb3f3865055d436971
jgoliszewski <jakub.goliszewski01@gmail.com>
2023-04-13 22:22:28 +0200

    add: controlls

--- camera.py
+++ camera.py
@@ -17,6 +17,38 @@ while run:
     # clock.tick(60)
 
     # controls
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_e] == True:  # E -> Move Forward
+        print("Move Forward")
+    if keys[pg.K_d] == True:  # D -> Move Back
+        print("Move Back")
+    if keys[pg.K_s] == True:  # S -> Move Left
+        print("Move Left")
+    if keys[pg.K_f] == True:  # F -> Move Right
+        print("Move Right")
+    if keys[pg.K_t] == True:  # R -> Move Up
+        print("Move Up")
+    if keys[pg.K_g] == True:  # W -> Move Down
+        print("Move Down")
+
+    if keys[pg.K_i] == True:  # I -> Tilt Down
+        print("Tilt Down")
+    if keys[pg.K_k] == True:  # K -> Tilt Up
+        print("Tilt Up")
+    if keys[pg.K_j] == True:  # J -> Pan Left
+        print("Pan Left")
+    if keys[pg.K_l] == True:  # L -> Pan Right
+        print("Pan Right")
+    if keys[pg.K_u] == True:  # U -> Roll Left
+        print("Roll Left")
+    if keys[pg.K_o] == True:  # O -> Roll Right
+        print("Rol Right")
+
+    if keys[pg.K_w] == True:  # W -> Zoom Out
+        print("Zoom out")
+    if keys[pg.K_r] == True:  # R -> Zoom In
+        print("Zoom in")
 
     # draw
     screen.fill(BACKGROUND)
