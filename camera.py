512823ea8f9994e69cbe5caf5bac97dadc922e27
jgoliszewski <jakub.goliszewski01@gmail.com>
2023-05-15 16:15:56 +0200

    edit: increase movement speed

--- camera.py
+++ camera.py
@@ -18,7 +18,7 @@ screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 init_screen(SCREEN_WIDTH, SCREEN_HEIGHT)
 init_cubes()
 
-z = 200
+z = 400
 font = pg.font.SysFont("Arial", 30)
 
 # clock = pg.time.Clock()
@@ -30,30 +30,30 @@ while run:
     keys = pg.key.get_pressed()
 
     if keys[pg.K_e] == True:  # E -> Move Forward
-        translate(0, 0, -0.2)
+        translate(0, 0, -0.5)
     if keys[pg.K_d] == True:  # D -> Move Back
-        translate(0, 0, 0.2)
+        translate(0, 0, 0.5)
     if keys[pg.K_s] == True:  # S -> Move Left
-        translate(0.2, 0, 0)
+        translate(0.5, 0, 0)
     if keys[pg.K_f] == True:  # F -> Move Right
-        translate(-0.2, 0, 0)
+        translate(-0.5, 0, 0)
     if keys[pg.K_g] == True:  # G -> Move Down
-        translate(0, 0.2, 0)
+        translate(0, 0.5, 0)
     if keys[pg.K_t] == True:  # T -> Move Up
-        translate(0, -0.2, 0)
+        translate(0, -0.5, 0)
 
     if keys[pg.K_i] == True:  # I -> Tilt Down
-        tilt(0.1)
+        tilt(0.3)
     if keys[pg.K_k] == True:  # K -> Tilt Up
-        tilt(-0.1)
+        tilt(-0.3)
     if keys[pg.K_j] == True:  # J -> Pan Left
-        pan(0.1)
+        pan(0.3)
     if keys[pg.K_l] == True:  # L -> Pan Right
-        pan(-0.1)
+        pan(-0.3)
     if keys[pg.K_u] == True:  # U -> Roll Left
-        roll(-0.1)
+        roll(-0.3)
     if keys[pg.K_o] == True:  # O -> Roll Right
-        roll(0.1)
+        roll(0.3)
 
     if keys[pg.K_w] == True:  # W -> Zoom Out
         if z > 100:
@@ -64,8 +64,7 @@ while run:
 
     #! draw
     screen.fill(BACKGROUND)
-    for i, cube in enumerate(cubes):
-        draw_cube(screen, cubes[i], COLORS[i % len(COLORS)], z)
+    draw_cubes(screen, cubes, z);
 
     zoom = font.render(f"Zoom: {round(z/200*100, 1)}%", True, COLORS[4])
     screen.blit(zoom, (10, 10))
